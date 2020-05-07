4e962699f66058a8db70bb00e588fe44bbb37f50
Codehouze <chijioke.emma.maxwell@gmail.com>
2020-05-07 19:53:48 +0100

    deploy

--- app.js
+++ app.js
@@ -4,14 +4,15 @@ const express = require ("express");
 const app = express();
 // Port Environment variable
 const PORT = process.env.PORT || 5000;
+const path = require ("path");
 
 if(!dev){
 
     app.disable('x-powered-by');
-    app.use(express.static(path.resolve(_dirname, 'build')));
+    app.use(express.static(path.join(__dirname, 'build')));
 
     app.get("*", (req,res)=>{
-    res.sendFile(path.resolve(_dirname, 'build', index.html));
+    res.sendFile(path.resolve(__dirname, 'build', index.html));
 });
 }
 
