925b9b96fdd3f2f8ce3178d5fcc0c4dbe65bc56a
Codehouze <chijioke.emma.maxwell@gmail.com>
2020-05-08 08:12:16 +0100

    Migrate from create-react-app-buildpack to Node server

--- app.js
+++ app.js
@@ -1,20 +1,43 @@
 
-const express = require ("express");
-// Creating the node server
-const app = express();
-// Port Environment variable
+const express = require('express');
+const path = require('path');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+
+const isDev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 5000;
-const path = require ("path");
 
-app.use(express.static(path.join(__dirname, 'build')))
+// Multi-process to utilize all CPU cores.
+if (!isDev && cluster.isMaster) {
+  console.error(`Node cluster master ${process.pid} is running`);
 
-app.get('/ping', (req, res) => {
-  return res.send('pong')
-})
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
+  cluster.on('exit', (worker, code, signal) => {
+    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
+  });
 
-app.listen(8000)
+} else {
+  const app = express();
 
+  // Priority serve any static files.
+  app.use(express.static(path.resolve(__dirname, 'client/build')));
+
+  // Answer API requests.
+  app.get('/api', function (req, res) {
+    res.set('Content-Type', 'application/json');
+    res.send('{"message":"Hello from the custom server!"}');
+  });
+
+  // All remaining requests return the React app, so it can handle routing.
+  app.get('*', function(request, response) {
+    response.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+  });
+
+  app.listen(PORT, function () {
+    console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`);
+  });
+}
