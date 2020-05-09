d68da9279d721334a9d0a9f5892c218bbed764a9
Codehouze <chijioke.emma.maxwell@gmail.com>
2020-05-09 13:40:26 +0100

    c

--- app.js
+++ app.js
@@ -1,27 +1,12 @@
 
 const express = require('express');
 const path = require('path');
-//const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
 const app = express();
 const isDev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 8000;
 
-// // Multi-process to utilize all CPU cores.
-// if (!isDev && cluster.isMaster) {
-//   console.error(`Node cluster master ${process.pid} is running`);
 
-//   // Fork workers.
-//   for (let i = 0; i < numCPUs; i++) {
-//     cluster.fork();
-//   }
-
-//   cluster.on('exit', (worker, code, signal) => {
-//     console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
-//   });
-
-// } else {
-  
 
   // Priority serve any static files.
   app.use(express.static(path.join(__dirname, 'client/build')));
